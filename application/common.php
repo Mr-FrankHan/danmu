976ded8d176eaa8c71ec9a9f488fa34db8820987
Mr-FrankHan <frankhan1992@163.com>
2019-01-18 14:49:04 +0800

    123

--- application/common.php
+++ application/common.php
@@ -1,12 +1,23 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006-2016 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: 流年 <liu21st@gmail.com>
-// +----------------------------------------------------------------------
 
 // 应用公共文件
+function &load_wechat_app()
+{
+    static $wechat;
+    if (!isset($wechat)) {
+        $config = [
+            'app_id' => "wx9063667218217d08",
+            'secret' => "8c2391e3ccfabee663aec6620af91a47",
+            // 指定 API 调用返回结果的类型：array(default)/collection/object/raw/自定义类名
+            'token' => '',          // Token
+            'response_type' => 'array',
+            "aes_key" => '',
+            'http' => [
+                'timeout' => 300.0,
+            ],
+        ];
+        $wechat = \EasyWeChat\Factory::officialAccount($config);
+    }
+
+    return $wechat;
+}
