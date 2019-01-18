2d2b4a06f06e636b1ff7158abc4b582603e18b12
Mr-FrankHan <frankhan1992@163.com>
2019-01-18 16:34:49 +0800

    123

--- application/index/controller/Message.php
+++ application/index/controller/Message.php
@@ -10,9 +10,13 @@ class Message extends Controller
     public function index()
     {
         $app = load_wechat_app();
+        $app->server->push(function ($message) {
+            return "您好！欢迎使用 EasyWeChat!";
+        });
+
         $response = $app->server->serve();
 
         // 将响应输出
-        $response->send();exit;
+        $response->send();
     }
 }
