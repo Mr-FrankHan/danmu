a59bf1a40e7516059a0765dc3d4a4a4f03a26b5e
Mr-FrankHan <frankhan1992@163.com>
2019-01-19 09:07:58 +0800

    123

--- application/common.php
+++ application/common.php
@@ -9,7 +9,7 @@ function &load_wechat_app()
             'app_id' => "wx9063667218217d08",
             'secret' => "8c2391e3ccfabee663aec6620af91a47",
             // 指定 API 调用返回结果的类型：array(default)/collection/object/raw/自定义类名
-            'token' => '',          // Token
+            'token' => '3FptRbZ6S809jCzeEi0hOTNePNiF26fR',          // Token
             'response_type' => 'array',
             "aes_key" => '',
             'http' => [
