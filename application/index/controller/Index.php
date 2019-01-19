40969bd18d7e0790510410c508f35a867a1de818
Mr-FrankHan <frankhan1992@163.com>
2019-01-19 10:56:35 +0800

    123

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,20 +1,12 @@
 <?php
 namespace app\index\controller;
 
-use EasyWeChat\Factory;
+use think\Controller;
 
-class Index
+class Index extends Controller
 {
     public function index()
     {
-        $app = load_wechat_app();
-        $app->server->push(function ($message) {
-            return "您好！欢迎使用 EasyWeChat!";
-        });
-
-        $response = $app->server->serve();
-
-        // 将响应输出
-        $response->send();
+        return view();
     }
 }
